13baef8c4d16f8376bb4ffd8b9e561198c91ebc5
FathimaJasama <jasamafathima01@gmail.com>
2024-01-11 09:02:51 +0530

    second commit

---
+++ views/header.php
@@ -0,0 +1,13 @@
+<?php
+include __DIR__ . '/../../config.php';
+pr('header');
+
+<html
+  lang="en"
+  class="light-style layout-menu-fixed"
+  dir="ltr"
+  data-theme="theme-default"
+  data-assets-path="assets/"
+  data-template="vertical-menu-template-free"
+>
+  <head>
